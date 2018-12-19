482eaa35e92c576c102bb9c540c478f457b34ffb
Sincky <30948192+Sincky@users.noreply.github.com>
2018-12-19 23:04:05 +0800

    增加登录后台后名字显示

--- application/admin/controller/Serindex.php
+++ application/admin/controller/Serindex.php
@@ -7,7 +7,8 @@ class Serindex extends Controller
 {
     public function serindex()
     {
-
+        $username = session('username');
+        $this->assign('username',$username);
     	return $this->fetch();
 	}
 
