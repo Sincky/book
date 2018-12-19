6803eeb5cb523b1d81dc4199a84124f6dd8952c5
Sincky <30948192+Sincky@users.noreply.github.com>
2018-12-19 19:06:44 +0800

    添加 商品"加入购物车"功能
    商品点击加入购物车，会向cart表中添加相关商品信息。

--- application/index/controller/Detail.php
+++ application/index/controller/Detail.php
@@ -10,6 +10,8 @@ namespace app\index\controller;
 
 use think\Controller;
 use think\Db;
+use app\index\model\Cart as CartModel;
+
 class Detail extends Controller
 {
     public function detail()
@@ -39,5 +41,38 @@ class Detail extends Controller
         $this->assign('comments',$comments);
 
         return $this->fetch();
+    }
+
+    public function addCart($bookID,$bookNum)
+    {
+        $email = session('email');
+        if(empty($email)){
+            $this->error('请先登录!','login/login');
+        }
+        $custID = Db::table('customer')->where('email',$email)->find()['custID'];
+        
+        $dbBook = CartModel::get(['custID'=>$custID,'bookID'=>$bookID]);
+        if(empty($dbBook)){
+            //如果查询为空,则说没有相关商品加入到购物车中
+            //加入商品条目到购物车
+            Db::transaction(function () use ($custID,$bookID){
+                CartModel::create(['custID'=>$custID,'bookID'=>$bookID,'bookNum'=>1]);
+            });
+
+            //跳转到购物车
+            //$this->redirect(url('cart/cart'));
+        }else{
+            $dbbookNum = $dbBook->getData('bookNum') + $bookNum;
+            //如果查询不为空，则说明已经有商品加入到购物车中
+            //只增加商品数量
+            $dbBook->bookNum = $dbbookNum;
+            $dbBook->save();
+
+            //跳转到购物车
+            //$this->redirect(url('cart/cart'));
+
+        }
+        echo '加入购物车成功！~';
+
     }
 }
