591bb121250edea86fda6dca9db74dcc55149fe5
Sincky <30948192+Sincky@users.noreply.github.com>
2018-12-21 20:58:38 +0800

    后台增加商品删除功能
    还有其他的一些小细节

--- public/static/js/product-add.js
+++ public/static/js/product-add.js
@@ -91,3 +91,36 @@ function update()
 	});
 
 }
+
+function productdelete() {
+	if(window.confirm('你确定要删除此商品吗？')){
+		//确定
+		var data = {};
+		data['bookID'] = $('#bookNumber').val();
+
+		$.ajax({
+			type:'POST',
+			url:'productdelete',
+			data: data,
+			success:function (data) {
+				var bool = data;
+				if(bool){
+					alert("删除商品成功！即将跳转到商品列表~");
+					window.location.href = "../productlist/productlist";
+				}else{
+					alert("删除失败，请重试~");
+				}
+			},
+			error:function (data) {
+				console.log(data);
+				alert("删除失败，请重试~");
+			}
+		});
+		return true;
+	}else{
+		//取消
+		return false;
+	}
+
+
+}
